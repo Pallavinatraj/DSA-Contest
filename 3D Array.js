46f26911cef0b0ad34044e826dcd5bb1320728f7
Pallavinatraj <pallavinatraj31@gmail.com>
2024-11-24 18:35:05 +0530

    DSA Problem3

---
+++ 3D Array.js
@@ -0,0 +1,25 @@
+// 3D Array
+let sum = "";
+function threeDArray() {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < input.length; j++) {
+            for (let k = 0; k < input.length; k++) {
+                sum += input[i][j][k] + " ";
+            }
+        }
+    }
+    console.log(sum);
+}
+
+
+input = [
+    [
+        [1, 2],
+        [3, 4]
+    ],
+    [
+        [5, 6],
+        [7, 8]
+    ]
+]
+threeDArray(input);
